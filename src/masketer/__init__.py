d1f0fd238646be61c3dc579c2afd0e19766655aa
ejimeoghenefejiro <ejimeoghenefejiro@yahoo.com>
2025-10-22 19:39:39 +0100

    change project name to mustafa from masketer

--- src/masketer/__init__.py
+++
@@ -1,9 +0,0 @@
-from .core import (
-    hello, master_data, country_analysis, sector_analysis, KPIs, corr_ana,
-    data, analysis, charts, countryname, rdata, df0, x,
-)
-__all__ = [
-    "hello","master_data","country_analysis","sector_analysis","KPIs","corr_ana",
-    "data","analysis","charts","countryname","rdata","df0","x",
-]
-
