f68c97a5797e13cf56cb54fcb054ce2c2c62f2c0
ejimeoghenefejiro <ejimeoghenefejiro@yahoo.com>
2025-10-22 19:47:47 +0100

    change folder for git action

---
+++ src/mustafa/__init__.py
@@ -0,0 +1,11 @@
+from .core import (
+    hello, master_data, country_analysis, sector_analysis, KPIs, corr_ana,
+    data, analysis, charts, countryname, rdata, df0, x,
+)
+__all__ = [
+    "hello","master_data","country_analysis","sector_analysis","KPIs","corr_ana",
+    "data","analysis","charts","countryname","rdata","df0","x",
+]
+
+
+
